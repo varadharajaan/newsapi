bbec755b6fb5ab39792d4c127c265d811e4a772b
Varadharajan Damotharan <varathu09@gmail.com>
2019-06-15 00:53:28 +0530

    adding war file  packaging

--- src/main/java/com/sapient/newsapi/sapientNewsApi/SapientNewsApiApplication.java
+++ src/main/java/com/sapient/newsapi/sapientNewsApi/SapientNewsApiApplication.java
@@ -2,14 +2,27 @@ package com.sapient.newsapi.sapientNewsApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.sapient.newsapi"})
-public class SapientNewsApiApplication {
+public class SapientNewsApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SapientNewsApiApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+		return builder.sources(SapientNewsApiApplication.class);
+	}
+	
+	@RequestMapping("/")
+	public String firstPage() {
+		return "Hello Sapient application successfully deployed and started...";	
+		
+	}
 }
